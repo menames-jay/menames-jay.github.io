c60f8cc2a7a9171ba4c9de5f01a3a6ebd1027ec7
menames-jay <118156798+menames-jay@users.noreply.github.com>
2023-08-21 23:02:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Create User Schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+});
+const User = mongoose.model('User', userSchema);
+
+app.use(express.json());
+
+// Signup Route
+app.post('/signup', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+// Login Route
+app.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user && await bcrypt.compare(req.body.password, user.password)) {
+      res.json({ message: 'Login successful' });
+    } else {
+      res.status(401).json({ error: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
